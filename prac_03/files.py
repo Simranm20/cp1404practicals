71adcf8c8f5faebdafd01e69dbd0fe43f6ecb76b
Simran.Mulchandani <simranmulchandani@my.jcu.edu.au>
2023-06-30 23:00:56 +1000

    files.py started as required

--- prac_03/files.py
+++ prac_03/files.py
@@ -16,28 +16,3 @@ with open("name.txt", 'r') as file:
     name = file.read()
 print("Your name is", name)
 
-# Program 3
-# Opening the file in read mode and reading the numbers
-with open("numbers.txt", 'r') as file:
-    number1 = int(file.readline())
-    number2 = int(file.readline())
-
-# Adding the numbers and printing the result
-result = number1 + number2
-print("The result is", result)
-
-# Program 4
-# Opening the file in read mode and reading all lines
-with open("numbers.txt", 'r') as file:
-    lines = file.readlines()
-
-# Initializing the total sum
-total = 0
-
-# Iterating over each line and adding the numbers to the total
-for line in lines:
-    number = int(line)
-    total += number
-
-# Printing the total sum
-print("The total is", total)
