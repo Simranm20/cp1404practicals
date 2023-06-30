79c4ba72f76c34e5e4f3cabc7f71d5fdea7000ad
Simran.Mulchandani <simranmulchandani@my.jcu.edu.au>
2023-06-30 21:37:29 +1000

    started total_income.py as required

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -1,5 +1,5 @@
 """
-CP1404/CP5632 Practical
+CP1404/CP5632 Practical 04
 Cumulative total income program
 """
 
@@ -9,21 +9,3 @@ def main():
     incomes = []
     number_of_months = int(input("How many number_of_months? "))
 
-    for month in range(1, number_of_months + 1):
-        income = float(input(f"Enter income for month {month}:"))
-        # income = float(input("Enter income for month " + str(month) + ": "))
-        incomes.append(income)
-
-    print_report(incomes)
-
-
-def print_report(incomes):
-    print("\nIncome Report\n-------------")
-    total = 0
-    for month, income in enumerate(incomes, 1):
-        total += income
-        # print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
-        print(f'Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}')
-
-
-main()
