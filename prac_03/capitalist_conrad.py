8a3de0fb0634d9e6badf4aa1fcda591bb0619d50
Simran.Mulchandani <simranmulchandani@my.jcu.edu.au>
2023-06-30 22:55:32 +1000

    capitalist_conrad.py completed as required

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -19,4 +19,30 @@ OUTPUT_FILE = "stock_price_info.txt"
 # open file for writing
 out_file = open(OUTPUT_FILE, 'w')
 
+price = INITIAL_PRICE
+print(f"Starting price: ${price:,.2f}", file=out_file)
+# print("${:,.2f}".format(price))
 
+number_of_days = 0
+
+while MIN_PRICE <= price <= MAX_PRICE:
+    number_of_days += 1
+    price_change = 0
+    # generate a random integer of 1 or 2
+    # if it's 1, the price increases, otherwise it decreases
+    if random.randint(1, 2) == 1:
+        # generate a random floating-point number
+        # between 0 and MAX_INCREASE
+        price_change = random.uniform(0, MAX_INCREASE)
+    else:
+        # generate a random floating-point number
+        # between negative MAX_DECREASE and 0
+        price_change = random.uniform(-MAX_DECREASE, 0)
+
+    price *= (1 + price_change)
+    # print("${:,.2f}".format(price))
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+print("Out of the loop.")
+
+# close the file
+out_file.close()
