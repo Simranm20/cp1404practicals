405df1fd51c8323a2f72acc014a5363725a4cd11
Simran.Mulchandani <simranmulchandani@my.jcu.edu.au>
2023-06-30 21:29:44 +1000

    modified list_exercises.py as required

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -22,4 +22,15 @@ print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the number is {sum(numbers) / len(numbers):.1f}")
 
+# 2 Woefully inadequate security checker
+
+userinput = str(input("Please enter your username "))
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+approved = [x for x in usernames if userinput in x]
+if approved:
+    print(f"Your username is {str(approved[0])} and your access is granted")
+else:
+    print(f"Access denied")
 
