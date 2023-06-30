7ffc25e6789be256b3258978ecb2b7a2f11b2f43
Simran.Mulchandani <simranmulchandani@my.jcu.edu.au>
2023-06-30 22:42:04 +1000

    started hex_colours.py as required

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical 05
+Hexadecimal colour lookup
+"""
+COLOR_NAME_TO_COLOR_CODE = {
+    "aliceblue": "#f0f8ff",
+    "aqua": "#00ffff",
+    "bittersweet shimmer": "#bf4f51",
+    "brick red": "#cb4154",
+    "bright lilac": "#d891ef",
+    "candy pink": "#e4717a",
+    "baby pink": "#f4c2c2",
+    "cerise": "#de3163",
+    "daffodil": "#ffff31",
+    "deeppink1": "#ff1493"
+}
+
