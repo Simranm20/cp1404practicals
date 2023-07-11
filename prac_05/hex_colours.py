b38c7c7a5ed3600b84c9e928c124ab68e5ec1bf9
jc957983 <simrankishin.mulchandni@my.jcu.edu.u>
2023-07-11 18:22:19 +1000

    started hex_colours.py as required

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -15,11 +15,3 @@ COLOR_NAME_TO_COLOR_CODE = {
     "deeppink1": "#ff1493"
 }
 
-color_name = input("Enter color name (hit enter to quit): ").lower()
-while color_name != "":
-    if color_name in COLOR_NAME_TO_COLOR_CODE:  # find key
-        print(f"{color_name} is {COLOR_NAME_TO_COLOR_CODE[color_name]}")
-    else:
-        print("Invalid color name")
-    color_name = input("Enter color name (hit enter to quit): ").lower()
-print("Bye.")
