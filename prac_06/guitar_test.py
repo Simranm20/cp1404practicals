2d1022f877445deb9ae8e2e3e4982b7056aaac6f
simranm20 <simrankishin.mulchnadnai@my.jcu.edu.au>
2023-07-25 15:48:43 +1000

    completed guitar_test.py as required

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -10,3 +10,14 @@ def test():
     year = int(input("Enter Year "))
     cost = input("Enter cost ")
 
+    guitar = Guitar(name, year, cost)
+    guitar2 = Guitar("Guitar 2 ", 1990, 1000)
+
+    print(f"{guitar.name} get_age() - Expected {0}. Got {guitar.get_age()}")
+    print(f"{guitar2.name} get_age() - Expected {0}. Got {guitar2.get_age()}")
+    print(f"{guitar.name} is_vintage() - Expected {0}. Got {guitar.is_vintage()}")
+    print(f"{guitar2.name} is_vintage() - Expected {0}. Got {guitar2.is_vintage()}")
+
+
+if __name__ == '__main__':
+    test()
