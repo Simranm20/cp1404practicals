9ee524fcd8383df72a1baca9d040c149e754a323
simranm20 <simrankishin.mulchnadnai@my.jcu.edu.au>
2023-07-25 15:47:32 +1000

    started guitar_test.py as required

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 Practical - 6
+Basic manual tests for Guitar class
+"""
+from guitar import Guitar
+
+
+def test():
+    name = input("Enter Name ")
+    year = int(input("Enter Year "))
+    cost = input("Enter cost ")
+
