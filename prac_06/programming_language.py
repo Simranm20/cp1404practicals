736fa18b3d090990c363fd2b2781baa7b58c71cb
simranm20 <simrankishin.mulchnadnai@my.jcu.edu.au>
2023-07-25 15:51:43 +1000

    programming_language.py completed as required

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -17,3 +17,21 @@ class ProgrammingLanguage:
     def __str__(self):
         return f'{self.name}, {self.typing} Typing, Reflection = {self.reflection}, First appeared in {self.year}'
 
+
+def run_tests():
+    """Run simple tests/demos on ProgrammingLanguage class."""
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, python, visual_basic]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    run_tests()
