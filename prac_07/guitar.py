e8ef7c021fd3c2019b5b7e9051dc41d874a29d48
simranm20 <simrankishin.mulchnadnai@my.jcu.edu.au>
2023-07-25 17:11:27 +1000

    started prac 7 as required

---
+++ prac_07/guitar.py
@@ -0,0 +1,17 @@
+class Guitar:
+    """Represent information about a guitar."""
+
+    def __init__(self, name, year, cost):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    # copy the idea from programming language
+
+    def __str__(self):
+        """Return string representation of a guitar."""
+        return f"{self.name}, Made in {self.year}, ${self.cost}"
+
+    def __lt__(self, other):
+        """sort guitar by year"""
+        return self.year < other.year
